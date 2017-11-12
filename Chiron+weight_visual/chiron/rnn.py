8f43d7a01c46dc1b08bd85319db0f65a5fe1d11a
etheleon <picy2k@gmail.com>
2017-11-12 08:05:01 +0000

    seq2seq + attention

--- Chiron+weight_visual/chiron/rnn.py
+++ Chiron+weight_visual/chiron/rnn.py
@@ -11,6 +11,7 @@ import numpy as np
 #from tensorflow.contrib.rnn.python.ops.core_rnn_cell import LSTMCell
 from tensorflow.contrib.rnn import LSTMCell
 from utils.lstm import BNLSTMCell
+from tensorflow.python.layers import core as layers_core
 from tensorflow.contrib.rnn.python.ops.rnn import stack_bidirectional_dynamic_rnn
 from summary import variable_summaries
 
@@ -54,11 +55,39 @@ def rnn_layers_one_direction(x,decoder_inputs,seq_length,training,hidden_num=200
         lasth,encoder_final_state= tf.nn.dynamic_rnn(cell_wrap,x,sequence_length = seq_length,dtype = tf.float32,scope = scope)
     #shape of lasth [batch_size,max_time,hidden_num*2]
     decoder_cell = BNLSTMCell(hidden_num,training)
-    print(encoder_final_state)
-    embeddings = tf.Variable(tf.random_uniform([3, 5], -1.0, 1.0), dtype=tf.float32)
-    decoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, decoder_inputs)
-    decoder_outputs, decoder_final_state = tf.nn.dynamic_rnn( decoder_cell, decoder_inputs_embedded,initial_state=encoder_final_state[2],dtype=tf.float32, scope="plain_decoder")
-    logits = tf.contrib.layers.linear(decoder_outputs, class_n)
+    #print(encoder_final_state)
+    embeddings  = tf.Variable(tf.random_uniform([3, 5], -1.0, 1.0), dtype=tf.float32)
+    decoder_emb_inp = tf.nn.embedding_lookup(embeddings, decoder_inputs)
+    helper = tf.contrib.seq2seq.TrainingHelper(
+           decoder_emb_inp, seq_length
+    )
+    # Decoder
+    projection_layer = layers_core.Dense(
+            class_n, use_bias=False
+    )
+    attention_states = tf.transpose(encoder_final_state[2], [1, 0, 2])
+    print(attention_states.shape)
+    attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+            200, attention_states,
+            memory_sequence_length=seq_length
+    )
+    decoder_cell = tf.contrib.seq2seq.AttentionWrapper(
+            decoder_cell, attention_mechanism,
+            attention_layer_size=200
+    )
+    initial_state = decoder_cell.zero_state(dtype=tf.float32, batch_size=64)
+    initial_state = initial_state.clone(cell_state=encoder_final_state[2])
+    decoder = tf.contrib.seq2seq.BasicDecoder(
+           decoder_cell, helper, initial_state,
+           output_layer=projection_layer
+    )
+    # Dynamic decoding
+    #print(decoder.shape)
+    outputs, final_context_state, _   = tf.contrib.seq2seq.dynamic_decode(decoder)
+    logits = outputs.rnn_output
+    #decoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, decoder_inputs)
+    #decoder_outputs, decoder_final_state = tf.nn.dynamic_rnn( decoder_cell, decoder_inputs_embedded,initial_state=encoder_final_state[2],dtype=tf.float32, scope="plain_decoder")
+    #logits = tf.contrib.layers.linear(decoder_outputs, class_n)
     #print(logits_fake.shape)
     #batch_size = lasth.get_shape().as_list()[0]
     #max_time = lasth.get_shape().as_list()[1]
