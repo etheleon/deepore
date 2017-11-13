42425f9b498211164a65f0f44062bb44f4111bb4
etheleon <picy2k@gmail.com>
2017-11-13 20:40:21 +0800

    update with original loss and fields for runraw

--- preprocessing/runraw.sh
+++ preprocessing/runraw.sh
@@ -3,8 +3,12 @@
 #output=/data/nanopore/out/rawoutput
 #ecoli
 
-fast5=/data/nanopore/data/ecoli/downloads/pass
-output=/data/nanopore/data/ecoli_raw
+#fast5=/data/nanopore/data/ecoli/downloads/pass
+#output=/data/nanopore/data/ecoli_raw
+
+fast5=/data/datasets/zika
+output=/data/nanopore/data/zika_raw
+
 chironDir=$HOME/github
 
 python $chironDir/chiron/utils/raw.py \
